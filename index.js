ea33c6b66d4a21fa9eb3f6b1df5d51f91b235f7b
Orbter <assouline39@gmail.com>
2023-12-02 01:09:28 +0200

    add modes as color and rainbow, and let u switch them

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const sketcher = document.getElementById("sketcher");
 const sketcher_size = sketcher.offsetHeight;
+let colorInput = document.getElementById("color-change");
 let penOn = false;
 const box = document.querySelector(".painter");
+const rainbow = document.getElementById("rainbow");
+let currentMode = "color";
+CreatBox(16, 16);
+rainbow.addEventListener("click", function () {
+  currentMode = "rainbow";
+});
 
-function ChangeColor() {
-  if (c) {
+function TogalMode() {
+  switch (currentMode) {
+    case "color":
+      currentMode = "color";
+      break;
+    case "rainbow":
+      currentMode = "rainbow";
+      break;
   }
 }
 
-CreatBox(16, 16);
+function ChangeColor() {
+  switch (currentMode) {
+    case "color":
+      return colorInput.value;
+    case "rainbow":
+      return rainbowColor();
+  }
+  console.log(colorInput.value);
+  return colorInput.value;
+}
+function rainbowColor() {
+  // Generate random values for red, green, and blue components
+  const randomRed = Math.floor(Math.random() * 256);
+  const randomGreen = Math.floor(Math.random() * 256);
+  const randomBlue = Math.floor(Math.random() * 256);
+
+  // Construct the color string in the format "rgb(r, g, b)"
+  const randomColor = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+
+  return randomColor;
+}
 function CreatBox(row, column) {
   for (let index = 0; index < row * column; index++) {
     let box = document.createElement("div");
@@ -17,7 +50,15 @@ function CreatBox(row, column) {
     box.style.height = sketcher_size / column + "px";
     box.addEventListener("mousedown", function () {
       penOn = true;
-      box.style.backgroundColor = "blue";
+      box.style.backgroundColor = ChangeColor();
+    });
+    box.addEventListener("mouseup", function () {
+      penOn = false;
+    });
+    box.addEventListener("mousemove", function () {
+      if (penOn) {
+        box.style.backgroundColor = ChangeColor();
+      }
     });
 
     sketcher.appendChild(box);
